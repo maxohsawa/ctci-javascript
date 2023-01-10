0fa5b8d7b00cea883fb324593c4a378d0c44af33
Max Ohsawa <max.ohsawa@gmail.com>
2023-01-10 12:18:16 +0200

    adds 1.8

--- ch1-arrays-and-strings/1.8-zero-matrix.js
+++ ch1-arrays-and-strings/1.8-zero-matrix.js
@@ -1,28 +1,46 @@
 function zeroMatrix(matrix) {
+  const coordsToZero = new Set();
+
   for (let y = 0; y < matrix.length; y++) {
     for (let x = 0; x < matrix[0].length; x++) {
       if (matrix[y][x] === 0) {
-        // up
-        for (let upY = y - 1; upY >= 0; y--) {
-          matrix[upY][x] = 0;
-        }
-        // right
-        for (let rightX = x + 1; rightX < matrix[0].length; x++) {
-          matrix[y][rightX] = 0;
-        }
-        // down
-        for (let downY = y + 1; downY < matrix.length; y++) {
-          matrix[(downY, x)] = 0;
-        }
-        // left
-        for (let leftX = x - 1; x >= 0; x--) {
-          matrix[(y, leftX)] = 0;
-        }
+        zeroCardinal(y, x, matrix, coordsToZero);
       }
     }
   }
 
+  for (const [y, x] of coordsToZero) {
+    matrix[y][x] = 0;
+  }
+
   return matrix;
+}
+
+function zeroCardinal(yEpicenter, xEpicenter, matrix, coordsToZero) {
+  let y = yEpicenter;
+  let x = xEpicenter;
+  // north
+  while (--y >= 0) {
+    coordsToZero.add([y, x]);
+  }
+  y = yEpicenter;
+
+  // east
+  while (++x < matrix[0].length) {
+    coordsToZero.add([y, x]);
+  }
+  x = xEpicenter;
+
+  // south
+  while (++y < matrix.length) {
+    coordsToZero.add([y, x]);
+  }
+  y = yEpicenter;
+
+  // west
+  while (--x >= 0) {
+    coordsToZero.add([y, x]);
+  }
 }
 
 function printMatrix(input) {
@@ -36,12 +54,27 @@ function printMatrix(input) {
   console.log('-------------------');
 }
 
-const test = [
-  [1, 1, 1],
-  [2, 2, 0],
-  [3, 3, 3],
+const tests = [
+  [
+    [1, 1, 1],
+    [2, 2, 0],
+    [3, 3, 3],
+  ],
+  [
+    [1, 1, 1, 5, 0],
+    [2, 2, 0, 6, 9],
+    [3, 3, 3, 1, 7],
+  ],
+  [
+    [1, 1, 1, 0, 1],
+    [2, 2, 0, 1, 1],
+    [3, 3, 3, 1, 0],
+  ],
+  [[1, 0]],
 ];
 
-printMatrix(test);
-const zeroedMatrix = zeroMatrix(test);
-printMatrix(zeroedMatrix);
+for (const test of tests) {
+  printMatrix(test);
+  const zeroedMatrix = zeroMatrix(test);
+  printMatrix(zeroedMatrix);
+}
